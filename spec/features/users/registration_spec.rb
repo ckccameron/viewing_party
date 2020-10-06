1d4a32ee22d165c3e10fea4396fd73361df78bba
Cam C <ckc.cameron@gmail.com>
2020-10-06 15:15:23 -0700

    Write spec for user registration
    Co-authored-by: Angela Guardia 
    <47278429+AngelaGuardia@users.noreply.github.com>
    Co-authored-by: Cam Chery <57038617+ckccameron@users.noreply.github.com>

---
+++ spec/features/users/registration_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe "user registration" do
+  it "allows a new user to register" do
+    visit root_path
+
+    click_link "Register"
+
+    expect(current_path).to eq("/register")
+
+    name = "Consuela"
+    email = "person@gmail.com"
+    password = "password123"
+    password_confirmation = "password123"
+
+    fill_in :name, with: name
+    fill_in :email, with: email
+    fill_in :password, with: password
+    fill_in :password_confirmation, with: password_confirmation
+
+    click_button "Create User"
+
+    expect(page).to have_content("Welcome, #{name}!")
+    expect(current_path).to eq("/dashboard")
+  end
+end
