01f25ec6b94d9d82c44f21352ca50e4112e81887
Cam C <ckc.cameron@gmail.com>
2020-10-06 15:19:08 -0700

    Add create action to UsersController with: * empty show route * strong params set up for create action
    Co-authored-by: Angela Guardia 
    <47278429+AngelaGuardia@users.noreply.github.com>
    Co-authored-by: Cam Chery <57038617+ckccameron@users.noreply.github.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,18 @@ class UsersController < ApplicationController
   def new
 
   end
+
+  def create
+    User.create(user_params)
+    redirect_to "/dashboard"
+  end
+
+  def show
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :email, :passsword, :password_confirmation)
+  end
 end
