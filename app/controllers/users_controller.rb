b532af028255125a6b59da4fe7836869d5347693
Cam C <ckc.cameron@gmail.com>
2020-10-06 19:45:34 -0700

    Add if/else logic to create action for UsersController
    user cannot register with:
    * email already in use
    * blank fields
    * password fields not matching
    
    Co-authored-by: Angela Guardia 
    <47278429+AngelaGuardia@users.noreply.github.com>
    Co-authored-by: Cam Chery <57038617+ckccameron@users.noreply.github.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,15 @@ class UsersController < ApplicationController
   end
 
   def create
-    new_user = User.create(user_params)
-    flash[:success] = "Welcome, #{new_user.name}!"
-    session[:user_id] = new_user.id
-    redirect_to "/dashboard"
+    new_user = User.new(user_params)
+    if new_user.save
+      flash[:success] = "Welcome, #{new_user.name}!"
+      session[:user_id] = new_user.id
+      redirect_to "/dashboard"
+    else
+      flash[:error] = new_user.errors.full_messages.uniq.to_sentence
+      redirect_to "/register"
+    end
   end
 
   def show
@@ -16,6 +21,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.permit(:name, :email, :passsword, :password_confirmation)
+    params.permit(:name, :email, :password, :password_confirmation)
   end
 end
