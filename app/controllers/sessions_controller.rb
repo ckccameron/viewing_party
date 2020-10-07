e2c1ae51b71d23fbf356e386669ae8c377df127b
Cam C <ckc.cameron@gmail.com>
2020-10-06 19:01:21 -0700

    Add create and destroy actions to SessionsController
    * create - user login
    * destroy - user logout
    
    Co-authored-by: Angela Guardia 
    <47278429+AngelaGuardia@users.noreply.github.com>
    Co-authored-by: Cam Chery <57038617+ckccameron@users.noreply.github.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,17 @@ class SessionsController < ApplicationController
   def new
 
   end
+
+  def create
+    user = User.find_by(email: params[:email])
+    session[:user_id] = user.id
+    flash[:success] = "Welcome, #{user.name}!"
+    redirect_to "/dashboard"
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:error] = "You have logged out!"
+    redirect_to "/"
+  end
 end
