a64aaa69deabff4e43151e7dc707f7464b063d02
Cam C <ckc.cameron@gmail.com>
2020-10-06 18:55:54 -0700

    Write spec for user logout
    Co-authored-by: Angela Guardia 
    <47278429+AngelaGuardia@users.noreply.github.com>
    Co-authored-by: Cam Chery <57038617+ckccameron@users.noreply.github.com>

--- spec/features/users/login_spec.rb
+++ spec/features/users/login_spec.rb
@@ -19,5 +19,32 @@ describe "user login" do
     expect(page).to have_content("Welcome, #{user.name}")
     expect(page).to have_button("Log out")
     expect(page).to_not have_button("Log in")
+
+    visit root_path
+
+    expect(page).to have_button("Log out")
+    expect(page).to_not have_button("Log in")
+    expect(page).to_not have_button("Register")
+  end
+
+  it "allows user to logout" do
+    user = create(:user)
+
+    visit root_path
+
+    click_button "Log in"
+
+    expect(current_path).to eq("/login")
+
+    fill_in :email, with: user.email
+    fill_in :password, with: user.password
+
+    click_button "Log in"
+
+    expect(current_path).to eq("/dashboard")
+
+    click_button "Log out"
+
+    expect(current_path).to eq("/")
   end
 end
