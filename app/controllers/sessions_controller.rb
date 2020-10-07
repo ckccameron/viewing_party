cfb00deefee772e69fd35caa6327b1a600fbda45
Cam C <ckc.cameron@gmail.com>
2020-10-06 19:54:20 -0700

    Fix RuboCop offenses
    Co-authored-by: Angela Guardia 
    <47278429+AngelaGuardia@users.noreply.github.com>
    Co-authored-by: Cam Chery <57038617+ckccameron@users.noreply.github.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,23 +1,21 @@
 class SessionsController < ApplicationController
-  def new
-
-  end
+  def new; end
 
   def create
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       flash[:success] = "Welcome, #{user.name}!"
-      redirect_to "/dashboard"
+      redirect_to '/dashboard'
     else
-      flash[:error] = "Sorry, your credentials are bad"
-      redirect_to "/login"
+      flash[:error] = 'Sorry, your credentials are bad'
+      redirect_to '/login'
     end
   end
 
   def destroy
     session[:user_id] = nil
-    flash[:error] = "You have logged out!"
-    redirect_to "/"
+    flash[:error] = 'You have logged out!'
+    redirect_to '/'
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,19 @@
 class UsersController < ApplicationController
-  def new
-
-  end
+  def new; end
 
   def create
     new_user = User.new(user_params)
     if new_user.save
       flash[:success] = "Welcome, #{new_user.name}!"
       session[:user_id] = new_user.id
-      redirect_to "/dashboard"
+      redirect_to '/dashboard'
     else
       flash[:error] = new_user.errors.full_messages.uniq.to_sentence
-      redirect_to "/register"
+      redirect_to '/register'
     end
   end
 
-  def show
-  end
+  def show; end
 
   private
 
