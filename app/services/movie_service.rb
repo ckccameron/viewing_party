a17eb64915d0420ccb97648cf95d528d7146dcfd
Angela Guardia <angelaguardia20@gmail.com>
2020-10-07 21:17:43 -0700

    Create MovieService Class * Add top_rated method * Add API connection and parser methods
    Co-authored-by: Angela Guardia 
    <47278429+AngelaGuardia@users.noreply.github.com>
    Co-authored-by: Cam Chery <57038617+ckccameron@users.noreply.github.com>

---
+++ app/services/movie_service.rb
@@ -0,0 +1,25 @@
+class MovieService
+  def top_rated
+    movies = []
+    i = 0
+    while movies.count < 40
+      i +=1
+      movies << to_json("movie/top_rated?page=#{i}")[:results]
+      movies.flatten!
+    end
+    movies
+  end
+
+  def to_json(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  private
+
+  def conn
+    Faraday.new(url: 'https://api.themoviedb.org/3/') do |req|
+      req.params['api_key'] = ENV['TMDB-API-KEY']
+    end
+  end
+end
