e19b81b3130b44eab93bdfdafdac95e69cfc1446
Angela Guardia <angelaguardia20@gmail.com>
2020-10-08 18:49:22 -0700

    Update VCR config api key naming
    Co-authored-by: Angela Guardia 
    <47278429+AngelaGuardia@users.noreply.github.com>
    Co-authored-by: Cam Chery <57038617+ckccameron@users.noreply.github.com>

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -75,7 +75,8 @@ end
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   config.hook_into :webmock
-  config.filter_sensitive_data('TMDB_API_KEY') { ENV['TMDB_API_KEY'] }
+  config.filter_sensitive_data('SECRET_TMDB_API_KEY') { ENV['TMDB_API_KEY'] }
   config.default_cassette_options = { re_record_interval: 7.days }
   config.configure_rspec_metadata!
+
 end
