c15d1fbee5b8afca49b6288c96df13fa825ac96b
Angela Guardia <angelaguardia20@gmail.com>
2020-10-08 18:52:32 -0700

    Refactor MovieService methods * Refactor top_rated method with params variable * Add params parameter to to_json method * Remove params from conn new block
    Co-authored-by: Angela Guardia 
    <47278429+AngelaGuardia@users.noreply.github.com>
    Co-authored-by: Cam Chery <57038617+ckccameron@users.noreply.github.com>

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -4,23 +4,25 @@ class MovieService
     page = 0
     while movies.size < 40
       page +=1
-      movies << to_json("movie/top_rated?page=#{page}")[:results]
+      params = { page: page }
+      movies << to_json("movie/top_rated", params)[:results]
       movies.flatten!
       break if movies == [nil]
     end
     movies
   end
 
-  def to_json(url)
-    response = conn.get(url)
+  def to_json(url, params)
+    response = conn.get(url) do |req|
+      req.params = params
+      req.params['api_key'] = ENV['TMDB_API_KEY']
+    end
     JSON.parse(response.body, symbolize_names: true)
   end
 
   private
 
   def conn
-    Faraday.new(url: 'https://api.themoviedb.org/3/') do |req|
-      req.params['api_key'] = ENV['TMDB_API_KEY']
-    end
+    Faraday.new(url: 'https://api.themoviedb.org/3/')
   end
 end
