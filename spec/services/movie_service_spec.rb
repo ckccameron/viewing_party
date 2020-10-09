8d2f06c9ae9c3fc23e900adbe858219290a28079
Angela Guardia <angelaguardia20@gmail.com>
2020-10-08 18:52:58 -0700

    Refactor MovieService test for robustness
    Co-authored-by: Angela Guardia 
    <47278429+AngelaGuardia@users.noreply.github.com>
    Co-authored-by: Cam Chery <57038617+ckccameron@users.noreply.github.com>

--- spec/services/movie_service_spec.rb
+++ spec/services/movie_service_spec.rb
@@ -14,8 +14,12 @@ describe MovieService do
       movies = @service.top_rated
 
       expect(movies.count).to eq(40)
+      expect(movies.first).to have_key(:title)
       expect(movies.first[:title]).to be_a(String)
+      expect(movies.first[:title].length).to be > 0
+      expect(movies.first).to have_key(:vote_average)
       expect(movies.first[:vote_average]).to be_a(Float)
+      expect(movies.first[:vote_average]).to be_between(0, 10).inclusive
     end
   end
 end
