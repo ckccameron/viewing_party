872c78c469973841419223518e82824e501f84ef
Angela Guardia <angelaguardia20@gmail.com>
2020-10-08 20:03:03 -0700

    Add test for MovieService search method
    Co-authored-by: Angela Guardia 
    <47278429+AngelaGuardia@users.noreply.github.com>
    Co-authored-by: Cam Chery <57038617+ckccameron@users.noreply.github.com>

--- spec/services/movie_service_spec.rb
+++ spec/services/movie_service_spec.rb
@@ -22,4 +22,19 @@ describe MovieService do
       expect(movies.first[:vote_average]).to be_between(0, 10).inclusive
     end
   end
+
+  it "returns up to 40 result for search" do
+    VCR.use_cassette("happy_search_results") do
+      movies = @service.search("happy")
+
+      expect(movies.count).to eq(40)
+      expect(movies.first).to have_key(:title)
+      expect(movies.first[:title]).to be_a(String)
+      expect(movies.first[:title]).to include("Happy")
+      expect(movies.first[:title].length).to be > 0
+      expect(movies.first).to have_key(:vote_average)
+      expect(movies.first[:vote_average]).to be_a(Float)
+      expect(movies.first[:vote_average]).to be_between(0, 10).inclusive
+    end
+  end
 end
