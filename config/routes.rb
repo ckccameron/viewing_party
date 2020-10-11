dc229fbe116d6d8c9befb243d4ca3ecb5af0a1af
Angela Guardia <angelaguardia20@gmail.com>
2020-10-10 18:29:16 -0700

    Add movie show route and new party route
    Co-authored-by: Angela Guardia 
    <47278429+AngelaGuardia@users.noreply.github.com>
    Co-authored-by: Cam Chery <57038617+ckccameron@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,7 @@ Rails.application.routes.draw do
   get '/discover', to: 'discover#index'
 
   get '/movies', to: 'movies#index'
+  get '/movies/:id', to: 'movies#show'
+
+  get '/parties/new', to: 'parties#new'
 end
