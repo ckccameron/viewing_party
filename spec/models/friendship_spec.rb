e5c270e2fe7b7ca2d860e3ed139ffd56021a8ebe
Cam C <ckc.cameron@gmail.com>
2020-10-11 16:49:57 -0700

    Write test for create_friendships_reciprocal method
    * Friendship model class method to create friendships array with user
    friendship and friend friendship
    
    Co-authored-by: Angela Guardia <47278429+AngelaGuardia@users.noreply.github.com>

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -4,4 +4,17 @@ describe Friendship do
   describe "relationships" do
     it {should belong_to(:user)}
   end
+
+  describe ".methods" do
+    it "can create friendship reciprocal" do
+      user1 = create(:user)
+      user2 = create(:user)
+
+      expect(Friendship.all.count).to eq(0)
+
+      Friendship.create_friendships_reciprocal(user1.id, user2.id)
+      
+      expect(Friendship.all.count).to eq(2)
+    end
+  end
 end
