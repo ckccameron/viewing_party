4ffeb270cd6b1aecd47105197aca2f29b0b72a9c
Cam C <ckc.cameron@gmail.com>
2020-10-11 15:11:46 -0700

    Add has_many associations to User model for friendships
    * has many friends through friendships

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,8 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, confirmation: true
 
+  has_many :friendships, dependent: :destroy
+  has_many :friends, through: :friendships
+
   has_secure_password
 end
