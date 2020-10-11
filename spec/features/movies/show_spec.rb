90f6e808d2a9266de5d9c88ce26f9c5c07f5570e
Angela Guardia <angelaguardia20@gmail.com>
2020-10-10 19:49:24 -0700

    Add test for movie info, details, cast, and reviews to Movie Show page
    Co-authored-by: Angela Guardia 
    <47278429+AngelaGuardia@users.noreply.github.com>
    Co-authored-by: Cam Chery <57038617+ckccameron@users.noreply.github.com>

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -53,5 +53,59 @@ describe 'As an authenticated user on' do
         end
       end
     end
+
+    it "movie info, details, cast and reviews" do
+      VCR.use_cassette("fight_club_top_rated_results") do
+        visit movies_path
+
+        fill_in :query, with: "Fight Club"
+        click_button "Search"
+        expect(current_path).to eq('/movies')
+
+        within('#movie-550') do
+          click_on "Fight Club"
+        end
+        VCR.use_cassette("fight_club_details_results") do
+          VCR.use_cassette("fight_club_cast_results") do
+            VCR.use_cassette("fight_club_review_results") do
+              expect(current_path).to eq('/movies/550')
+
+              expect(page).to have_css(".movie-title")
+              title = find(".movie-title").text
+              expect(title).not_to be_empty
+
+              expect(page).to have_css(".movie-vote-average")
+              vote_average = find(".movie-vote-average").text
+              expect(vote_average).not_to be_empty
+
+              expect(page).to have_css(".movie-runtime")
+              runtime = find(".movie-runtime").text
+              expect(runtime).not_to be_empty
+
+              expect(page).to have_css(".movie-genres")
+              genres = find(".movie-genres").text
+              expect(genres).not_to be_empty
+
+              expect(page).to have_css(".movie-overview")
+              overview = find(".movie-overview").text
+              expect(overview).not_to be_empty
+
+              expect(page).to have_css(".movie-review-count")
+              review_count = find(".movie-review-count").text
+              expect(review_count).not_to be_empty
+
+              expect(page).to have_css(".movie-cast")
+              cast = page.all(".movie-cast")
+              expect(cast.first.text).not_to be_empty
+              expect(cast.size).to eq(10)
+
+              expect(page).to have_css(".movie-review")
+              review = page.all(".movie-review").first.text
+              expect(review).not_to be_empty
+            end
+          end
+        end
+      end
+    end
   end
 end
