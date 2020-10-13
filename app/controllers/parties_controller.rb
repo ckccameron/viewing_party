fb1dd7fc22ac97fccb05f8390c6c033e888a6a15
Angela Guardia <angelaguardia20@gmail.com>
2020-10-13 06:47:21 -0700

    Add create action to PartiesController * Format params to match Party attributes * Create Guest invites for successful party creations * Add sad path flash message
    Co-authored-by: Angela Guardia 
    <47278429+AngelaGuardia@users.noreply.github.com>
    Co-authored-by: Cam Chery <57038617+ckccameron@users.noreply.github.com>

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -7,6 +7,17 @@ class PartiesController < ApplicationController
   end
 
   def create
-    redirect_to dashboard_path
+    attributes = Party.format_params(params)
+    @party = Party.new(attributes)
+    if @party.save
+      flash[:success] = "Your party has been created!"
+      Guest.create_invites(params, current_user, @party)
+      redirect_to dashboard_path
+    else
+      flash[:error] = @party.errors.full_messages.to_sentence
+      redirect_to new_party_path
+    end
   end
+
+  ## QUESTION: How could we use strong params here? are the class methods here conventional?
 end
