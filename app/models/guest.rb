a35f6ad3ec7ed059a63810db9fbc37fa726bd9bb
Angela Guardia <angelaguardia20@gmail.com>
2020-10-13 06:47:21 -0700

    Create Guest Model
    Co-authored-by: Angela Guardia 
    <47278429+AngelaGuardia@users.noreply.github.com>
    Co-authored-by: Cam Chery <57038617+ckccameron@users.noreply.github.com>

---
+++ app/models/guest.rb
@@ -0,0 +1,17 @@
+class Guest < ApplicationRecord
+  belongs_to :party
+  belongs_to :user
+
+  validates :is_host, inclusion: { in: [true, false] }
+
+  def self.create_invites(party_params, current_user, party)
+    Guest.create({ user_id: current_user.id, party_id: party.id, is_host: true })
+    guests_emails = party_params[:party][:guests]
+    guests_emails.delete("")
+    guests_emails.each do |email|
+      guest = User.find_by(email: email)
+      Guest.create({ user_id: guest.id, party_id: party.id })
+    end
+  end
+  ## QUESTION: How could we refactor the method above?
+end
