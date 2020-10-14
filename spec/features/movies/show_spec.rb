befd68c835a2b766924d61618fec25e0f4a910e8
Cam C <ckc.cameron@gmail.com>
2020-10-13 19:58:35 -0700

    Clean up movie show spec
    Co-authored-by: Angela Guardia <47278429+AngelaGuardia@users.noreply.github.com>

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -1,23 +1,5 @@
 require 'rails_helper'
 
-# As an authenticated user,
-# When I visit the movie's detail page,
-# I should see
-#
-#  Button to create a viewing party
-# Details This button should take the authenticated user to the new event page
-#
-# And I should see the following information about the movie:
-#
-#  Movie Title
-#  Vote Average of the movie
-#  Runtime in hours & minutes
-#  Genere(s) associated to movie
-#  Summary description
-#  List the first 10 cast members (characters&actress/actors)
-#  Count of total reviews
-#  Each review's author and information
-
 describe 'As an authenticated user on' do
   describe 'the Movies Details page I should see' do
     before(:each) do
@@ -33,7 +15,7 @@ describe 'As an authenticated user on' do
       VCR.use_cassette("top_40_rated_movies", allow_playback_repeats: true) do
         visit movies_path
 
-        VCR.use_cassette("inception_search_results") do
+        VCR.use_cassette("inception_search_results", allow_playback_repeats: true) do
           fill_in :query, with: "Inception"
           click_button "Search"
           expect(current_path).to eq('/movies')
