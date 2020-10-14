aca14740220dc425626c88aa64d24a0c23b064a3
Cam C <ckc.cameron@gmail.com>
2020-10-13 19:56:46 -0700

    Clean up new party spec
    Co-authored-by: Angela Guardia <47278429+AngelaGuardia@users.noreply.github.com>

--- spec/features/parties/new_spec.rb
+++ spec/features/parties/new_spec.rb
@@ -1,16 +1,5 @@
 require 'rails_helper'
 
-# As an authenticated user,
-# When I visit the new viewing party page,
-# I should see a form with the following:
-#
-#   Movie Title (that is un-editable)
-#   Duration of Party with a default value of movie runtime in minutes
-#   When: field to select date
-#   Start Time: field to select time
-#   Checkboxes next to each friend (if user has friends)
-#   Button to create a party
-
 describe "As an authenticated user" do
   describe "new party page" do
     before :each do
@@ -36,19 +25,16 @@ describe "As an authenticated user" do
             click_link "Fight Club"
           end
 
-          expect(current_path).to eq("/movies/550")
-
           click_button "Create Viewing Party"
 
           expect(current_path).to eq("/parties/new")
 
-          # expect(page).to have_content("Fight Club")
           expect(page).to have_field(:duration)
           expect(page).to have_css("#party_date_1i")
           expect(page).to have_css("#party_date_2i")
           expect(page).to have_css("#party_date_3i")
-          expect(page).to have_css("#party_time_4i")
-          expect(page).to have_css("#party_time_5i")
+          expect(page).to have_css("#party_date_4i")
+          expect(page).to have_css("#party_date_5i")
           expect(page).to have_css(".party-friends")
 
           within ".party-friends" do
@@ -60,20 +46,18 @@ describe "As an authenticated user" do
             expect(page).to have_content(friend2.email)
           end
 
-          select "2020", from: "party_date_1i"
-          select "October", from: "party_date_2i"
-          select "12", from: "party_date_3i"
-          # select "10", from: "party_time_4i"
-          # select "15", from: "party_time_5i"
+          select "2021", from: "party_date_1i"
+          select "January", from: "party_date_2i"
+          select "31", from: "party_date_3i"
+          select "10", from: "party_date_4i"
+          select "45", from: "party_date_5i"
           check "#{friend1.name}"
           check "#{friend2.name}"
+
           click_button "Create This Party"
 
           expect(current_path).to eq(dashboard_path)
-
-          within ".parties" do
-            expect(page).to have_content("Fight Club")
-          end
+          expect(page).to have_content("Your party has been created!")
         end
       end
     end
